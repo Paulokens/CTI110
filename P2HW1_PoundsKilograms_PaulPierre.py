b19688cc72825da401ccd6fe1ee6e2fce3d60a71
Paulokens <52183606+Paulokens@users.noreply.github.com>
2019-06-24 21:47:27 -0400

    Add files via upload
    Second submission

---
+++ P2HW1_PoundsKilograms_PaulPierre.py
@@ -0,0 +1,14 @@
+# Pounds to Kilograms converter
+# 06/19/2019
+# CTI-110 P2HW1 - Pounds to Kilogram Converter
+# Pierre Paul
+#
+
+# Get the pounds.
+lb = float(input('Enter the pounds: '))
+
+# Convert the pounds to kilograms.
+kg = lb / 2.2046
+
+# Display the result.
+print( lb, 'pound(s) is equivalent to', format(kg, ',.2f'), 'kilogram(s)')
