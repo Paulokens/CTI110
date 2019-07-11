42f2f605f9b25edc0e299ab4d75d4e6049a0db8e
Paulokens <52183606+Paulokens@users.noreply.github.com>
2019-07-10 23:50:42 -0400

    Add files via upload
    Nested Loops submission

---
+++ P4HW4_Pierre.py
@@ -0,0 +1,15 @@
+# Nested Loops
+# 07/10/2019
+# CTI-110 P4HW4 - Nested Loops
+# Paul Pierre
+#
+
+import turtle
+t = turtle.Turtle()
+t.color("blue")
+
+for i in range (1,3):
+  for j in range (1,5):
+      t.left(90)
+      t.forward(200)
+  t.right(6)
